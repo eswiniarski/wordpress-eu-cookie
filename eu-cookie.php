41a3a8823152581e73994a2aa974bf407687077d
Emil Świniarski <emil.swiniarski7@gmail.com>
2018-08-21 22:08:59 +0200

    up

--- eu-cookie.php
+++ eu-cookie.php
@@ -6,39 +6,47 @@
  * Author: Emil Świniarski
  */
 
- define(EU_COOKIE_DIR_PATH, plugin_dir_path(__FILE__));
- define(EU_COOKIE_DIR_URL, plugin_dir_url( __FILE__ ));
+if (false === defined('ABSPATH')) {
+    exit;
+};
 
- function registerSettingsFields() {
+$eu_cookie_dir_url = plugin_dir_url(__FILE__);
+$eu_cookie_dir_path = plugin_dir_path(__FILE__);
 
- }
+if (false === is_admin()) {
+    add_action('wp_enqueue_scripts', 'eu_cookie_register_assets', 100);
+    add_action('wp_footer', 'eu_cookie_load_message_html', 100);
+} else {
+    add_action('admin_menu', 'eu_cookie_menu');
+}
 
- function loadSettings() {
-
- }
-
- function getExcludedSites() {
-
- }
-
- if (false === is_admin()) {
-    add_action('wp_enqueue_scripts', 'registerAssets', 100);
-    add_action('wp_footer', 'loadMessageHtml', 100);
- }
-
- function registerAssets() {
-    wp_register_style('eu-cookie', EU_COOKIE_DIR_URL . 'css/eu-cookie.css');
+function eu_cookie_register_assets() {
+    wp_register_style('eu-cookie', $GLOBALS['eu_cookie_dir_url'] . 'css/eu-cookie.css');
     wp_enqueue_style('eu-cookie');
 
-    wp_register_script('eu-cookie', EU_COOKIE_DIR_URL . 'js/eu-cookie.js');
+    wp_register_script('eu-cookie', $GLOBALS['eu_cookie_dir_url'] . 'js/eu-cookie.js');
     wp_enqueue_script('eu-cookie');
- }
+}
 
-function loadMessageHtml() {
+function eu_cookie_load_message_html() {
     ob_start();
-    include(EU_COOKIE_DIR_PATH . 'template/default.php');
+    include($GLOBALS['eu_cookie_dir_path'] . 'template/default.php');
     $content = ob_get_contents();
     ob_end_clean();
 
     echo $content;
+}
+
+function eu_cookie_menu() {
+	add_options_page(
+		'EU Cookie',
+		'EU Cookie',
+		'manage_options',
+		'eu-cookie.php',
+		'eu_cookie_page'
+	);
+}
+
+function eu_cookie_page() {
+    echo 'EU Cookie settings';
 }
